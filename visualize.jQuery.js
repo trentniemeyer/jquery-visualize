e8c691a76c70651191fe5143893a98390833fd18
Emanuele Aina <emanuele.aina@dndg.it>
2009-10-27 23:59:15 +0100

    Fix infinite loop on tall charts with small range
    If the total vertical range is narrow enough that the number of
    labels exceed its value, Visualize would enter in a infinite loop.
    
    Ensuring that the pixel interval is at least 1px fixes the issue.

--- visualize.jQuery.js
+++ visualize.jQuery.js
@@ -161,6 +161,7 @@ $.fn.visualize = function(options, container){
 					var numLabels = Math.round(chartHeight / 30);
 					//var totalRange = this.topValue() + Math.abs(this.bottomValue());
 					var loopInterval = Math.round(this.totalYRange() / Math.floor(numLabels)); //fix provided from lab
+					loopInterval = Math.max(loopInterval, 1);
 					for(var j=this.bottomValue(); j<=topValue; j+=loopInterval){
 						yLabels.push(j); 
 					}
