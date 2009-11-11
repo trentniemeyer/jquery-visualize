c089d18ac717cd440aff90b2d4d4f60a0c8ff4dc
Andrew Magee <amagee@gmail.com>
2009-11-11 09:49:25 +0100

    Improve long labels support on horizontal bar charts
    Support really long labels on the left of a horizontal bar chart
    (short ones are centred properly, longer ones will span multiple
    lines, but won't overflow the space allocated to their bar).

--- visualize.jQuery.js
+++ visualize.jQuery.js
@@ -370,14 +370,43 @@ $.fn.visualize = function(options, container){
 					var label = thisLi.find('span:not(.line)').addClass('label');
 
 					if (horizontal) {
-						thisLi.css({'top': liBottom * i, 'height': liBottom});
-						label.css('height', liBottom);
+						/**
+						 * For left labels, we want to vertically align the text
+						 * to the middle of its container, but we don't know how
+						 * many lines of text we will have, since the labels could
+						 * be very long.
+						 *
+						 * So we set a min-height of liBottom, and a max-height
+						 * of liBottom + 1, so we can then check the label's actual
+						 * height to determine if it spans one line or more lines.
+						 */
+						label.css({
+							'min-height': liBottom,
+							'max-height': liBottom + 1,
+							'vertical-align': 'middle'
+						});
+						thisLi.css({'top': liBottom * i, 'min-height': liBottom});
 
-						// This is needed for the vertical align to work properly:
-						// http://www.ampsoft.net/webdesign-l/vertical-aligned-nav-list.html
-						label.css('line-height', parseInt(liBottom) + 'px');
-
-						label.css('vertical-align', 'middle');
+						r = label[0].getClientRects()[0];
+						if (r.bottom - r.top == liBottom) {
+							/* This means we have only one line of text; hence
+							 * we can centre the text vertically by setting the line-height,
+							 * as described at:
+							 *   http://www.ampsoft.net/webdesign-l/vertical-aligned-nav-list.html
+							 *
+							 * (Although firefox has .height on the rectangle, IE doesn't,
+							 * so we use r.bottom - r.top rather than r.height.)
+							 */
+							label.css('line-height', parseInt(liBottom) + 'px');
+						}
+						else {
+							/*
+							 * If there is more than one line of text, then we shouldn't
+							 * touch the line height, but we should make sure the text
+							 * doesn't overflow the container.
+							 */
+							label.css("overflow", "hidden");
+						}
 					}
 					else {
 						thisLi.css('bottom', liBottom * i).prepend('<span class="line" />');
